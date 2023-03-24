d82b02798b527dab6a06d3f0ecbae33077c0c5b9
polandonion <125926220+polandonion@users.noreply.github.com>
2023-03-24 13:44:13 +0100

    Create wedrowki_dlugosc_podejscia.cpp

---
+++ WILK/wedrowki_dlugosc_podejscia.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+vector <int> vec;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int n;
+    cin >> n;
+
+    while (n --) {
+        int x;
+        cin >> x;
+
+        vec.push_back(x);
+    }
+
+    int mxAkt = 0, mx = 0;
+    for (int i = 1; i < vec.size(); i ++) {
+        if (vec[i] >= vec[i - 1]) {
+            mxAkt ++;
+            mx = max(mx, mxAkt);
+        }
+        else
+            mxAkt = 0;
+    }
+
+    cout << mx;
+}
